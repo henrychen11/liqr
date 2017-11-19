d424a246f21ca41e1986ad2ce847b67a5ba24c0a
henrychen11 <henrychen1@gmail.com>
2017-11-19 00:56:44 -0800

    create users migration

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,36 @@
+class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+
+    if @user == current_user && @user.update_attributes(user_params)
+      render :show
+    elsif !@user
+      render json: ['User not found'], status: 404
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+  private
+
+    def ensure_session_token
+      self.session_token ||= User.generate_session_token
+    end
+
+  end
