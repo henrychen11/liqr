e275f5d39b6c3101c481837caa4e9a570a73e663
henrychen11 <henrychen1@gmail.com>
2017-11-23 11:24:38 -0800

    rename welcome container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import Welcome from './welcome/welcome';
+import WelcomeContainer from './welcome/welcome_container';
 import NavBarContainer from './navbar/NavBarContainer';
 import { Route, Switch } from 'react-router-dom';
 import SessionFormContainer from './SessionFormContainer';
 import PhotoIndexContainer from './photos/photo_index_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import Footer from './footer';
+
 
 const App = () => (
   <div>
     <NavBarContainer />
     <main>
       <Switch>
-        <Route exact path="/" component={Welcome} />
+        <Route exact path="/" component={WelcomeContainer} />
         <AuthRoute path="/login" component={SessionFormContainer} />
         <AuthRoute path="/signup" component={SessionFormContainer} />
         <ProtectedRoute path="/home" component={PhotoIndexContainer} />
       </Switch>
     </main>
 
-  <footer>
-    <Footer />
-  </footer>
+
 </div>
 );
 
