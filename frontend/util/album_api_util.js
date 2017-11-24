8ff31bdaee5bf2b5c5f664580fe6cfb7dbd4054e
henrychen11 <henrychen1@gmail.com>
2017-11-23 22:07:28 -0800

    added album API utils

---
+++ frontend/util/album_api_util.js
@@ -0,0 +1,36 @@
+export const fetchAlbums = (userId) => (
+  $.ajax({
+    method: 'GET',
+    url: `api/users/${userId}/albums`
+  })
+);
+
+export const fetchAlbum = (albumId) => (
+  $.ajax({
+    method: 'GET',
+    url: `api/albums/${albumId}`
+  })
+);
+
+export const createAlbum = (album) => (
+  $.ajax({
+    method: 'POST',
+    url: 'api/albums',
+    data: { album }
+  })
+);
+
+export const updateAlbum = (album) => (
+  $.ajax({
+    method: 'PATCH',
+    url: `api/albums/${album.id}`,
+    data: { album }
+  })
+);
+
+export const deleteAlbum = (albumId) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `api/albums/${albumId}`,
+  })
+);
