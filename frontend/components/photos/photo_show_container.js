8301c36769f9234baab391fa2653e98c814dae76
henrychen11 <henrychen1@gmail.com>
2017-11-25 13:58:39 -0800

    added photo show container

--- frontend/components/photos/photo_show_container.js
+++ frontend/components/photos/photo_show_container.js
@@ -5,4 +5,15 @@ import { requestPhoto, editPhoto, destroyPhoto } from './actions/photo_actions';
 
 import { withRouter } from 'react-router-dom';
 
-const mapStateToProps = (state) =
+const mapStateToProps = (state, ownProps) => ({
+  photo: state.entities.photos[ownProps.match.params.photoId],
+  currentUser: state.session.currentUser
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  requestPhoto: (photoId) => dispatch(requestPhoto(photoId)),
+  editPhoto: (photoId) => dispatch(editPhoto(photoId)),
+  destroyPhoto: (photoId) => dispatch(destroyPhoto(photoId))
+});
+
+export default connect(withRouter(mapStateToProps, mapDispatchToProps)(PhotoShow));
