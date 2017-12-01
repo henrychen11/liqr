637db121cb986f50f1061610ec469a93caaa12cc
henrychen11 <henrychen1@gmail.com>
2017-11-30 16:43:24 -0800

    adding in spinners

--- frontend/components/photos/photo_index.jsx
+++ frontend/components/photos/photo_index.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import PhotoIndexItem from './photo_index_item';
 import Masonry from 'react-masonry-component';
+import Spinner from './spinner';
 
 class PhotoIndex extends React.Component {
 
-  componentWillMount(){
-    this.props.requestAllPhotos();
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount(){
+    this.props.requestAllPhotos().then( () => this.setState({ loading: false }));
   }
 
   render() {
     const {photos} = this.props;
-    return (
-      <div>
-        <h2 className="page-header">Explore</h2>
+
+    if (this.state.loading){
+      return (
+          <Spinner />
+      );
+    } else {
+      return (
+        <div>
+          <h2 className="page-header">Explore</h2>
           <Masonry
             className={'my-gallery-class'}
             >
-              {
-                photos.map( (photo, idx) =>
-                <PhotoIndexItem  key={idx} photo={photo} />
-              )}
+            {
+              photos.map( (photo, idx) =>
+              <PhotoIndexItem  key={idx} photo={photo} />
+            )}
           </Masonry>
-      </div>
-    );
+        </div>
+      );
+    }
   }
 }
 
--- frontend/components/photos/photo_user_index.jsx
+++ frontend/components/photos/photo_user_index.jsx
@@ -1,37 +1,49 @@
 import React from 'react';
 import PhotoIndexItem from './photo_index_item';
 import Masonry from 'react-masonry-component';
+// import Spinner from './spinner';
+import { RingLoader } from 'react-spinners';
 
 class PhotoUserIndex extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+
   componentWillMount(){
-    this.props.requestUserPhotos(this.props.currentUser.id);
+    this.props.requestUserPhotos(this.props.currentUser.id).then( () => this.setState({ loading: false }));
   }
 
   render() {
     const {photos} = this.props;
 
+    if (this.state.loading){
       return (
-        <div>
-          { (photos.length === 0) ? <div className="loader"></div> :
-            <div>
-              <h2 className="page-header">My Pictures</h2>
-              <Masonry
-                className={'my-gallery-class'}
-                elementType={'ul'}
-                disableImagesLoaded={false}
-                updateOnEachImageLoad={false}
-                >
-                {
-                  photos.map( (photo, idx) =>
-                  <PhotoIndexItem  key={idx} photo={photo} />
-                )}
-              </Masonry>
-            </div>
-          }
-        </div>
+        <Spinner />
       );
-    }
-  }
+    } else {
 
+      return (
+              <div>
+                <h2 className="page-header">My Pictures</h2>
+                <Masonry
+                  className={'my-gallery-class'}
+                  elementType={'ul'}
+                  disableImagesLoaded={false}
+                  updateOnEachImageLoad={false}
+                  >
+                  {
+                    photos.map( (photo, idx) =>
+                    <PhotoIndexItem  key={idx} photo={photo} />
+                  )}
+                </Masonry>
+              </div>
+            );
+        }
+      }
+    }
 export default PhotoUserIndex;
